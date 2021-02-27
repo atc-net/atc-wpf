c7c39e5dccc7f8bb0a7eb91ec2a966e10f128310
David Kallesen <david@lmdk.dk>
2021-02-27 14:50:14 +0100

    Fix SA1111

--- sample/Atc.Wpf.Sample/AssemblyInfo.cs
+++ sample/Atc.Wpf.Sample/AssemblyInfo.cs
@@ -2,5 +2,4 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
-    ResourceDictionaryLocation.SourceAssembly
-)]
+    ResourceDictionaryLocation.SourceAssembly)]
