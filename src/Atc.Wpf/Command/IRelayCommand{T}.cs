2cec1887331baca3d1cbf4998eac764a59c528e9
David Kallesen <david@lmdk.dk>
2022-08-28 01:05:03 +0200

    fix: S2326 for interface

--- src/Atc.Wpf/Command/IRelayCommand{T}.cs
+++ src/Atc.Wpf/Command/IRelayCommand{T}.cs
@@ -2,6 +2,7 @@
 // ReSharper disable UnusedTypeParameter
 namespace Atc.Wpf.Command;
 
+[SuppressMessage("Major Code Smell", "S2326:Unused type parameters should be removed", Justification = "OK.")]
 public interface IRelayCommand<T> : ICommand
 {
     /// <summary>
