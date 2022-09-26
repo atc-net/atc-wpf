feb9d505acde410f9f35dfb1d189ee82c2291036
David Kallesen <david@lmdk.dk>
2022-09-26 21:56:33 +0200

    feat: Add SolidColorBrushExtensions

---
+++ src/Atc.Wpf/Extensions/SolidColorBrushExtensions.cs
@@ -0,0 +1,27 @@
+// ReSharper disable once CheckNamespace
+// ReSharper disable ConvertIfStatementToSwitchStatement
+namespace System.Windows.Media;
+
+public static class SolidColorBrushExtensions
+{
+    // ReSharper disable once UnusedParameter.Global
+    public static SolidColorBrush GetFromHex(
+        this SolidColorBrush solidColorBrush,
+        string hexValue)
+    {
+        ArgumentNullException.ThrowIfNull(solidColorBrush);
+        ArgumentNullException.ThrowIfNull(hexValue);
+
+        if (!hexValue.StartsWith("#", StringComparison.Ordinal) &&
+            hexValue.Length is 3 or 6 or 8)
+        {
+            hexValue = $"#{hexValue}";
+        }
+        else if (!hexValue.StartsWith("#", StringComparison.Ordinal))
+        {
+            throw new ArgumentException("It is not hex value", nameof(hexValue));
+        }
+
+        return new SolidColorBrush((Color)ColorConverter.ConvertFromString(hexValue));
+    }
+}
