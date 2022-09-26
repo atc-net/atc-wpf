8552ad758561ef3fb23a6310828567595c97762d
David Kallesen <david@lmdk.dk>
2022-09-26 22:22:53 +0200

    feat: Add StandardNumericFormatTypeToFormatStringValueConverter and enum: StandardNumericFormatType

---
+++ src/Atc.Wpf/Enums/StandardNumericFormatType.cs
@@ -0,0 +1,83 @@
+// ReSharper disable once CheckNamespace
+namespace Atc.Wpf.Controls;
+
+/// <summary>
+/// StandardNumericFormatType.
+/// </summary>
+public enum StandardNumericFormatType
+{
+    /// <summary>
+    /// The general.
+    /// </summary>
+    General,
+
+    /// <summary>
+    /// The currency.
+    /// </summary>
+    Currency,
+
+    /// <summary>
+    /// The decimal1.
+    /// </summary>
+    Decimal1,
+
+    /// <summary>
+    /// The decimal2.
+    /// </summary>
+    Decimal2,
+
+    /// <summary>
+    /// The decimal3.
+    /// </summary>
+    Decimal3,
+
+    /// <summary>
+    /// The decimal4.
+    /// </summary>
+    Decimal4,
+
+    /// <summary>
+    /// The fixed point1.
+    /// </summary>
+    FixedPoint1,
+
+    /// <summary>
+    /// The fixed point2.
+    /// </summary>
+    FixedPoint2,
+
+    /// <summary>
+    /// The fixed point3.
+    /// </summary>
+    FixedPoint3,
+
+    /// <summary>
+    /// The fixed point4.
+    /// </summary>
+    FixedPoint4,
+
+    /// <summary>
+    /// The number1.
+    /// </summary>
+    Number1,
+
+    /// <summary>
+    /// The number2.
+    /// </summary>
+    Number2,
+
+    /// <summary>
+    /// The number3.
+    /// </summary>
+    Number3,
+
+    /// <summary>
+    /// The number4.
+    /// </summary>
+    Number4,
+
+    /// <summary>
+    /// The percent.
+    /// </summary>
+    Percent,
+}
