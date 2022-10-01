d71717c84936f7990c1744ba765d1ea94ebc479c
David Kallesen <david@lmdk.dk>
2022-10-02 00:21:38 +0200

    feat: Styled Button (default)

---
+++ sample/Atc.Wpf.Theming.Sample/Samples/InputButton/ButtonView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Theming.Sample.Samples.InputButton;
+
+/// <summary>
+/// Interaction logic for ButtonView.
+/// </summary>
+public partial class ButtonView
+{
+    public ButtonView()
+    {
+        InitializeComponent();
+    }
+}
