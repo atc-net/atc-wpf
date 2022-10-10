98d8c6a263dd0752c3dcb27da042ad70f8698f68
David Kallesen <david@lmdk.dk>
2022-10-10 18:11:26 +0200

    fix: Remove dublicate "SelectedKey"

--- src/Atc.Wpf.Controls/LabelControls/Abstractions/ILabelComboBox.cs
+++ src/Atc.Wpf.Controls/LabelControls/Abstractions/ILabelComboBox.cs
@@ -3,6 +3,4 @@ namespace Atc.Wpf.Controls.LabelControls.Abstractions;
 public interface ILabelComboBox : ILabelComboBoxBase
 {
     Dictionary<string, string> Items { get; set; }
-
-    string SelectedKey { get; set; }
 }
