92e82f316176c1f860a08e06d7428fec7b03f08a
David Kallesen <david@lmdk.dk>
2022-10-18 11:12:15 +0200

    feat: Improve HideAreas for Visibility

---
+++ src/Atc.Wpf.Controls/LabelControls/Internal/ValueConverters/LabelControlHideAreasToBoolValueConverter.cs
@@ -0,0 +1,33 @@
+namespace Atc.Wpf.Controls.LabelControls.Internal.ValueConverters;
+
+/// <summary>
+/// ValueConverter: Label-Control HideAreas To bool.
+/// </summary>
+[ValueConversion(typeof(LabelControlHideAreasType), typeof(bool))]
+internal class LabelControlHideAreasToBoolValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not LabelControlHideAreasType currentHideAreasType)
+        {
+            throw new InvalidEnumArgumentException(nameof(value), 0, typeof(LabelControlHideAreasType));
+        }
+
+        if (parameter is not LabelControlHideAreasType requiredHideAreasType)
+        {
+            throw new InvalidEnumArgumentException(nameof(value), 0, typeof(LabelControlHideAreasType));
+        }
+
+        if (requiredHideAreasType == LabelControlHideAreasType.None)
+        {
+            return true;
+        }
+
+        return !currentHideAreasType.HasFlag(requiredHideAreasType);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException("This is a OneWay converter.");
+    }
+}
