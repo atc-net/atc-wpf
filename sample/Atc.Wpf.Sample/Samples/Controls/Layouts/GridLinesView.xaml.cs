25e7ec4205127a73d3224f084f0185cee4c9d68f
David Kallesen <david@lmdk.dk>
2022-11-02 23:59:41 +0100

    feat: Add GridLines

---
+++ sample/Atc.Wpf.Sample/Samples/Controls/Layouts/GridLinesView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.Samples.Controls.Layouts;
+
+/// <summary>
+/// Interaction logic for GridLinesView.xaml
+/// </summary>
+public partial class GridLinesView
+{
+    public GridLinesView()
+    {
+        InitializeComponent();
+    }
+}
