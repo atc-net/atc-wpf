a72a3d57ac2429258f143820913c7bacf2effb9e
David Kallesen <david@lmdk.dk>
2022-11-05 16:22:03 +0100

    feat: Add ReversibleStackPanel

---
+++ sample/Atc.Wpf.Sample/Samples/Controls/Layouts/ReversibleStackPanelView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.Samples.Controls.Layouts;
+
+/// <summary>
+/// Interaction logic for ReversibleStackPanelView.
+/// </summary>
+public partial class ReversibleStackPanelView
+{
+    public ReversibleStackPanelView()
+    {
+        InitializeComponent();
+    }
+}
