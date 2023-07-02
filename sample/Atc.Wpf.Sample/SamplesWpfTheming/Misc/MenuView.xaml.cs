c26543abbaac699906ffa92b994d08499614f50c
David Kallesen <david@lmdk.dk>
2023-07-03 00:17:45 +0200

    feat: Add Menu & MenuItem styles

---
+++ sample/Atc.Wpf.Sample/SamplesWpfTheming/Misc/MenuView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.SamplesWpfTheming.Misc;
+
+/// <summary>
+/// Interaction logic for MenuView.
+/// </summary>
+public partial class MenuView
+{
+    public MenuView()
+    {
+        InitializeComponent();
+    }
+}
