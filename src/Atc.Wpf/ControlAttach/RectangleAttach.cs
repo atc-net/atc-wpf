db15c42c2995df3fa879c63135bd9d981a953586
David Kallesen <david@lmdk.dk>
2023-08-09 22:35:00 +0200

    feat: Add RectangleAttach

---
+++ src/Atc.Wpf/ControlAttach/RectangleAttach.cs
@@ -0,0 +1,52 @@
+namespace Atc.Wpf.ControlAttach;
+
+[SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "OK.")]
+public static class RectangleAttach
+{
+    public static readonly DependencyProperty CircularProperty = DependencyProperty.RegisterAttached(
+        "Circular",
+        typeof(bool),
+        typeof(RectangleAttach),
+        new PropertyMetadata(
+            BooleanBoxes.FalseBox,
+            OnCircularChanged));
+
+    private static void OnCircularChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not System.Windows.Shapes.Rectangle rectangle)
+        {
+            return;
+        }
+
+        if ((bool)e.NewValue)
+        {
+            var binding = new MultiBinding
+            {
+                Converter = new RectangleCircularValueConverter(),
+            };
+
+            binding.Bindings.Add(new Binding(FrameworkElement.ActualWidthProperty.Name) { Source = rectangle });
+            binding.Bindings.Add(new Binding(FrameworkElement.ActualHeightProperty.Name) { Source = rectangle });
+            rectangle.SetBinding(System.Windows.Shapes.Rectangle.RadiusXProperty, binding);
+            rectangle.SetBinding(System.Windows.Shapes.Rectangle.RadiusYProperty, binding);
+        }
+        else
+        {
+            BindingOperations.ClearBinding(rectangle, FrameworkElement.ActualWidthProperty);
+            BindingOperations.ClearBinding(rectangle, FrameworkElement.ActualHeightProperty);
+            BindingOperations.ClearBinding(rectangle, System.Windows.Shapes.Rectangle.RadiusXProperty);
+            BindingOperations.ClearBinding(rectangle, System.Windows.Shapes.Rectangle.RadiusYProperty);
+        }
+    }
+
+    public static void SetCircular(
+        DependencyObject element,
+        bool value)
+        => element.SetValue(CircularProperty, BooleanBoxes.Box(value));
+
+    public static bool GetCircular(
+        DependencyObject element)
+        => (bool)element.GetValue(CircularProperty);
+}
