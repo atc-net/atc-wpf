9db7d6157bf257fb2b071cf42296af98e7cfc6e4
David Kallesen <david@lmdk.dk>
2023-11-01 23:25:16 +0100

    feat: Improve JPropertyTypeToColorValueConverter to support JTokenType of Date, Guid, Uri

--- src/Atc.Wpf.Controls/Constants.cs
+++ src/Atc.Wpf.Controls/Constants.cs
@@ -8,6 +8,9 @@ public static class Constants
     public const string JTokenHexColorFloat = "#ad7fa8";
     public const string JTokenHexColorInteger = "#ad7fa8";
     public const string JTokenHexColorBoolean = "#c4a000";
+    public const string JTokenHexColorDate = "#f4a000";
+    public const string JTokenHexColorGuid = "#f4af00";
+    public const string JTokenHexColorUri = "#f4a0f0";
     public const string JTokenHexColorNull = "#ff6e00";
 
     public static Brush JTokenColorString => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorString)!;
@@ -17,6 +20,12 @@ public static class Constants
     public static Brush JTokenColorInteger => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorInteger)!;
 
     public static Brush JTokenColorBoolean => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorBoolean)!;
+
+    public static Brush JTokenColorDate => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorDate)!;
+
+    public static Brush JTokenColorGuid => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorGuid)!;
+
+    public static Brush JTokenColorUri => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorUri)!;
 
     public static Brush JTokenColorNull => (Brush)new BrushConverter().ConvertFrom(JTokenHexColorNull)!;
 }
