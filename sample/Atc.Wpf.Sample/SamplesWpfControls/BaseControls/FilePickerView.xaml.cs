9cab7c62a7a78aef9ec6853e9ac17557dff791cb
David Kallesen <david@lmdk.dk>
2023-11-26 22:34:02 +0100

    feat: WIP on FilePicker, DirectoryPicker, LabelFilePicker, LabelDirectoryPicker

---
+++ sample/Atc.Wpf.Sample/SamplesWpfControls/BaseControls/FilePickerView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.SamplesWpfControls.BaseControls;
+
+/// <summary>
+/// Interaction logic for FilePickerView.
+/// </summary>
+public partial class FilePickerView
+{
+    public FilePickerView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ sample/Atc.Wpf.Sample/SamplesWpfControls/LabelControls/LabelDirectoryPickerView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.SamplesWpfControls.LabelControls;
+
+/// <summary>
+/// Interaction logic for LabelDirectoryPickerView.
+/// </summary>
+public partial class LabelDirectoryPickerView
+{
+    public LabelDirectoryPickerView()
+    {
+        InitializeComponent();
+    }
+}
