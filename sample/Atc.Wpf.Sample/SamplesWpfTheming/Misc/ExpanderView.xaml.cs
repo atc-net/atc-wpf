76f2538d2f6b60187701f60e53bf4ecaf18ab473
David Kallesen <david@lmdk.dk>
2023-11-28 23:10:49 +0100

    feat: Add style for control Expander

---
+++ sample/Atc.Wpf.Sample/SamplesWpfTheming/Misc/ExpanderView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.SamplesWpfTheming.Misc;
+
+/// <summary>
+/// Interaction logic for ExpanderView.
+/// </summary>
+public partial class ExpanderView
+{
+    public ExpanderView()
+    {
+        InitializeComponent();
+    }
+}
