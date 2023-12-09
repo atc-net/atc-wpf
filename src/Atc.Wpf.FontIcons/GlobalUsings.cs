b276d27e5f98f02888c9bdca006fe7e7e54175ec
David Kallesen <david@lmdk.dk>
2023-12-09 20:24:46 +0100

    chore: Cleanup

--- src/Atc.Wpf.FontIcons/GlobalUsings.cs
+++ src/Atc.Wpf.FontIcons/GlobalUsings.cs
@@ -6,5 +6,4 @@ global using System.Windows.Controls;
 global using System.Windows.Data;
 global using System.Windows.Markup;
 global using System.Windows.Media;
-
-global using Atc.Wpf.Controls;
+global using System.Windows.Media.Imaging;
