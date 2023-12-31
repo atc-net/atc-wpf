134510223bb533fa942f8b684d81773d28fbf804
David Kallesen <david@lmdk.dk>
2023-12-31 19:19:11 +0100

    feat: Add FontFamilySelectorView & LabelFontFamilySelectorView

---
+++ sample/Atc.Wpf.Sample/SamplesWpfControls/LabelControls/LabelFontFamilySelectorView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Sample.SamplesWpfControls.LabelControls;
+
+/// <summary>
+/// Interaction logic for LabelFontFamilySelectorView.
+/// </summary>
+public partial class LabelFontFamilySelectorView
+{
+    public LabelFontFamilySelectorView()
+    {
+        InitializeComponent();
+    }
+}
