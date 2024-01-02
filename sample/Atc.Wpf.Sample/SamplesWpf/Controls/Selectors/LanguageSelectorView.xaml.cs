21f6c2b0335dfcfef3a385e3282823bc57b83903
David Kallesen <david@lmdk.dk>
2024-01-02 16:18:23 +0100

    chore: Cleanup

--- sample/Atc.Wpf.Sample/SamplesWpf/Controls/Selectors/LanguageSelectorView.xaml.cs
+++ sample/Atc.Wpf.Sample/SamplesWpf/Controls/Selectors/LanguageSelectorView.xaml.cs
@@ -2,7 +2,7 @@ namespace Atc.Wpf.Sample.SamplesWpf.Controls.Selectors;
 
 /// <summary>
 /// Interaction logic for LanguageSelectorView.
-/// </summary<>
+/// </summary>
 public partial class LanguageSelectorView
 {
     public LanguageSelectorView()
