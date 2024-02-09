6ab1cf19eee3b973f0175be18a74e39a59fca0c8
David Kallesen <david@lmdk.dk>
2024-02-09 16:30:26 +0100

    feat: Add enum: SelectionMode

---
+++ src/Atc.Wpf/Enums/SelectionMode.cs
@@ -0,0 +1,10 @@
+// ReSharper disable once CheckNamespace
+namespace Atc.Wpf;
+
+[SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "OK.")]
+public enum SelectionMode
+{
+    None,
+    Single,
+    Multiple,
+}
