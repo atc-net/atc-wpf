c531cbf8b45ae0602a10e76627f680b285af78c1
David Kallesen <david@lmdk.dk>
2024-03-04 15:49:12 +0100

    feat: Expose ButtonsAlignment for LabelNumberControl's

--- src/Atc.Wpf.Controls/LabelControls/LabelNumberControl.cs
+++ src/Atc.Wpf.Controls/LabelControls/LabelNumberControl.cs
@@ -2,6 +2,20 @@ namespace Atc.Wpf.Controls.LabelControls;
 
 public class LabelNumberControl : LabelControl, ILabelNumberControl
 {
+    public static readonly DependencyProperty ButtonsAlignmentProperty = DependencyProperty.Register(
+        nameof(ButtonsAlignment),
+        typeof(ButtonsAlignment),
+        typeof(LabelNumberControl),
+        new FrameworkPropertyMetadata(
+            ButtonsAlignment.Right,
+            FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    public ButtonsAlignment ButtonsAlignment
+    {
+        get => (ButtonsAlignment)GetValue(ButtonsAlignmentProperty);
+        set => SetValue(ButtonsAlignmentProperty, value);
+    }
+
     public static readonly DependencyProperty HideUpDownButtonsProperty = DependencyProperty.Register(
         nameof(HideUpDownButtons),
         typeof(bool),
