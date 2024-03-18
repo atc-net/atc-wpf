2f327a5c3a62b25223935a84e73f915f8ed412f3
David Kallesen <david@lmdk.dk>
2024-03-18 19:58:20 +0100

    feat: Add BasicApplicationOptions, RecentOpenFileOption and RecentOpenFilesOption

---
+++ src/Atc.Wpf.Controls/Options/RecentOpenFilesOption.cs
@@ -0,0 +1,12 @@
+// ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+namespace Atc.Wpf.Controls.Options;
+
+public class RecentOpenFilesOption
+{
+    public const string SectionName = "RecentOpenFiles";
+
+    public IList<RecentOpenFileOption> RecentOpenFiles { get; init; } = [];
+
+    public override string ToString()
+        => $"{nameof(RecentOpenFiles)}.Count: {RecentOpenFiles?.Count}";
+}
