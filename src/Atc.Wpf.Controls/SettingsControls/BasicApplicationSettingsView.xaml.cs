5567019e6cf767050dee16b73b1b73d66f6a9d5b
David Kallesen <david@lmdk.dk>
2024-03-18 19:57:48 +0100

    feat: Add BasicApplicationSettingsView and BasicApplicationSettingsViewModel

---
+++ src/Atc.Wpf.Controls/SettingsControls/BasicApplicationSettingsView.xaml.cs
@@ -0,0 +1,12 @@
+namespace Atc.Wpf.Controls.SettingsControls;
+
+/// <summary>
+/// Interaction logic for BasicApplicationSettingsView.
+/// </summary>
+public partial class BasicApplicationSettingsView
+{
+    public BasicApplicationSettingsView()
+    {
+        InitializeComponent();
+    }
+}
