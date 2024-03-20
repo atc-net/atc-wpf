5ad43101945501996331070e3c3314c642d18493
David Kallesen <david@lmdk.dk>
2024-03-21 00:56:38 +0100

    feat: Add LabelControlBaseExtensions -> FindByIdentifier

--- src/Atc.Wpf.Controls/LabelControls/Extensions/LabelControlBaseExtensions.cs
+++ src/Atc.Wpf.Controls/LabelControls/Extensions/LabelControlBaseExtensions.cs
@@ -6,4 +6,15 @@ public static class LabelControlBaseExtensions
     public static bool IsValid(
         this ILabelControlBase labelControl)
         => labelControl is not ILabelControl control || control.IsValid();
+
+    public static T? FindByIdentifier<T>(
+        this List<ILabelControlBase> labelControls,
+        string identifier)
+        where T : class, ILabelControlBase
+    {
+        ArgumentNullException.ThrowIfNull(labelControls);
+        ArgumentException.ThrowIfNullOrEmpty(identifier);
+
+        return labelControls.Find(x => x.Identifier == identifier) as T;
+    }
 }
