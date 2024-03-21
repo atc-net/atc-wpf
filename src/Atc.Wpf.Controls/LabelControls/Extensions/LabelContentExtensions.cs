e72f8ec83166c95f8101d7a33e94be34c9638db5
David Kallesen <david@lmdk.dk>
2024-03-22 00:19:09 +0100

    feat: Add LabelContentExtensions

---
+++ src/Atc.Wpf.Controls/LabelControls/Extensions/LabelContentExtensions.cs
@@ -0,0 +1,16 @@
+// ReSharper disable CheckNamespace
+namespace Atc.Wpf.Controls.LabelControls;
+
+public static class LabelContentExtensions
+{
+    public static T? GetViewModel<T>(
+        this LabelContent labelContent)
+        where T : ViewModelBase
+    {
+        ArgumentNullException.ThrowIfNull(labelContent);
+
+        return labelContent.Content is FrameworkElement frameworkElement
+            ? frameworkElement.DataContext as T
+            : null;
+    }
+}
