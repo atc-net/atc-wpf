f3b464a94d8233230b494c0254b654c06df29dad
David Kallesen <david@lmdk.dk>
2025-02-10 20:39:50 +0100

    feat: improve StringExtensions and add StringExtensionsTests

--- src/Atc.Wpf.SourceGenerators/Extensions/StringExtensions.cs
+++ src/Atc.Wpf.SourceGenerators/Extensions/StringExtensions.cs
@@ -1,57 +1,114 @@
 namespace Atc.Wpf.SourceGenerators.Extensions;
 
-internal static class StringExtensions
+public static class StringExtensions
 {
     public static string EnsureFirstCharacterToUpper(
         this string value)
-        => value.Length switch
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        return value.Length switch
         {
             0 => value,
             1 => value.ToUpperInvariant(),
             _ => char.ToUpperInvariant(value[0]) + value.Substring(1),
         };
+    }
 
     public static string[] ExtractAttributeParameters(
         this string value)
     {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
         var start = value.IndexOf('(');
+        if (start == -1)
+        {
+            return [value];
+        }
+
         var end = value.LastIndexOf(')');
-        if (start == -1 || end == -1)
+        if (end == -1)
         {
             return [value];
         }
 
         var parameterString = value.Substring(start + 1, end - start - 1);
-        var parameterList = parameterString.Split(',');
-
         var parameters = new List<string>();
-        foreach (var parameter in parameterList)
+
+        var currentParam = new StringBuilder();
+        var insideBrackets = false;
+
+        foreach (var c in parameterString)
         {
-            var trimmedParameter = parameter
-                .Replace("\"", string.Empty)
-                .Trim();
-
-            if (trimmedParameter.Length <= 0)
+            insideBrackets = c switch
             {
-                continue;
-            }
+                '[' => true,
+                ']' => false,
+                _ => insideBrackets,
+            };
 
-            if (value.StartsWith("nameof(", StringComparison.Ordinal))
+            if (c == ',' && !insideBrackets)
             {
-                var valueStart = value.IndexOf('(') + 1;
-                var valueEnd = value.LastIndexOf(')') - valueStart;
-                trimmedParameter = value.Substring(valueStart, valueEnd).Trim();
-            }
+                var trimmedParam = currentParam.ToString().Replace("\"", string.Empty).Trim();
 
-            parameters.Add(trimmedParameter);
+                if (trimmedParam.Length > 0)
+                {
+                    parameters.Add(trimmedParam);
+                }
+
+                currentParam.Clear();
+            }
+            else
+            {
+                currentParam.Append(c);
+            }
+        }
+
+        var lastParam = currentParam.ToString().Replace("\"", string.Empty).Trim();
+
+        if (lastParam.Length > 0)
+        {
+            parameters.Add(lastParam);
         }
 
         return parameters.ToArray();
     }
 
+    public static string ExtractParameterValue(
+        this string value)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        var indexOfEqual = value.IndexOf('=');
+        if (indexOfEqual != -1)
+        {
+            value = value.Substring(indexOfEqual);
+        }
+
+        return value
+            .Replace("=", string.Empty)
+            .Replace("[", string.Empty)
+            .Replace("]", string.Empty)
+            .Trim();
+    }
+
     public static string StripPrefixFromField(
         this string fieldName)
     {
+        if (fieldName is null)
+        {
+            throw new ArgumentNullException(nameof(fieldName));
+        }
+
         if (fieldName.StartsWith("m_", StringComparison.Ordinal))
         {
             fieldName = fieldName.Substring(2);
