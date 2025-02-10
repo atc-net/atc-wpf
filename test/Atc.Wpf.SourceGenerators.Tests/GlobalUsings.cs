b6e25ee2b375ad392caee555b3059045d69e4468
David Kallesen <david@lmdk.dk>
2025-02-10 20:39:04 +0100

    chore: cleanup

--- test/Atc.Wpf.SourceGenerators.Tests/GlobalUsings.cs
+++ test/Atc.Wpf.SourceGenerators.Tests/GlobalUsings.cs
@@ -1,4 +1,5 @@
 global using System.Diagnostics.CodeAnalysis;
+global using Atc.Wpf.SourceGenerators.Extensions;
 global using Atc.Wpf.SourceGenerators.Generators;
 global using Atc.XUnit;
 global using Microsoft.CodeAnalysis;
