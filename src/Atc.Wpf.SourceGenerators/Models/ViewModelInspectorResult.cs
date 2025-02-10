5a00436110fd0e934b76dc4cc68fb742392226b4
David Kallesen <david@lmdk.dk>
2025-02-10 16:09:45 +0100

    chore: add ToString on models

--- src/Atc.Wpf.SourceGenerators/Models/ViewModelInspectorResult.cs
+++ src/Atc.Wpf.SourceGenerators/Models/ViewModelInspectorResult.cs
@@ -11,4 +11,7 @@ internal sealed class ViewModelInspectorResult(
     public bool FoundAnythingToGenerate
         => RelayCommandsToGenerate.Count > 0 ||
            PropertiesToGenerate.Count > 0;
+
+    public override string ToString()
+        => $"{nameof(RelayCommandsToGenerate)}.Count: {RelayCommandsToGenerate.Count}, {nameof(PropertiesToGenerate)}.Count: {PropertiesToGenerate.Count}, {nameof(FoundAnythingToGenerate)}: {FoundAnythingToGenerate}";
 }
