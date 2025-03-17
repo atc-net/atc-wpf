6b8d579a6f44f2993f42f8710a7bb28a46c7839d
David Kallesen <david@lmdk.dk>
2025-03-17 10:35:47 +0100

    feat: add CreateInformation

---
+++ src/Atc.Wpf.Controls/Monitoring/ApplicationEventEntryFactory.cs
@@ -0,0 +1,19 @@
+namespace Atc.Wpf.Controls.Monitoring;
+
+public static class ApplicationEventEntryFactory
+{
+    public static ApplicationEventEntry CreateInformation(
+        string area,
+        string message)
+        => new(LogCategoryType.Information, area, message);
+
+    public static ApplicationEventEntry CreateWarning(
+        string area,
+        string message)
+        => new(LogCategoryType.Warning, area, message);
+
+    public static ApplicationEventEntry CreateError(
+        string area,
+        string message)
+        => new(LogCategoryType.Warning, area, message);
+}
