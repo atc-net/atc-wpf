2863ef1c3e3f9a38d013acac433ddcb7445de6fb
David Kallesen <david@lmdk.dk>
2025-04-05 21:59:11 +0200

    chore: remove AttachedPropertyAttribute & DependencyPropertyAttribute (moved to xamltoolkit.wpf)

--- src/Atc.Wpf/Controls/Attributes/AttachedPropertyAttribute{T}.cs
+++
@@ -1,6 +0,0 @@
-// ReSharper disable CheckNamespace
-namespace Atc.Wpf;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class AttachedPropertyAttribute<T>(string propertyName)
-    : DependencyPropertyAttribute<T>(propertyName);
