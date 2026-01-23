0b87e0ecb9ff866c3de59caa322c5ac89c738305
davidkallesen <david@lmdk.dk>
2026-01-23 20:41:54 +0100

    refactor(converters): replace ThicknessSideType with LeftTopRightBottomType from Atc package

--- src/Atc.Wpf/ValueConverters/ThicknessBindingValueConverter.cs
+++ src/Atc.Wpf/ValueConverters/ThicknessBindingValueConverter.cs
@@ -3,12 +3,12 @@ namespace Atc.Wpf.ValueConverters;
 /// <summary>
 /// ValueConverter: Thickness To Thickness binding.
 /// </summary>
-[ValueConversion(typeof(Thickness), typeof(Thickness), ParameterType = typeof(ThicknessSideType))]
+[ValueConversion(typeof(Thickness), typeof(Thickness), ParameterType = typeof(LeftTopRightBottomType))]
 public sealed class ThicknessBindingValueConverter : IValueConverter
 {
     public static readonly ThicknessBindingValueConverter Instance = new();
 
-    public ThicknessSideType IgnoreThicknessSide { get; set; } = ThicknessSideType.None;
+    public LeftTopRightBottomType IgnoreThicknessSide { get; set; } = LeftTopRightBottomType.None;
 
     /// <inheritdoc />
     public object? Convert(
@@ -24,17 +24,17 @@ public sealed class ThicknessBindingValueConverter : IValueConverter
 
         var ignoreThickness = IgnoreThicknessSide;
 
-        if (parameter is ThicknessSideType sideType)
+        if (parameter is LeftTopRightBottomType sideType)
         {
             ignoreThickness = sideType;
         }
 
         return ignoreThickness switch
         {
-            ThicknessSideType.Left => new Thickness(0, thickness.Top, thickness.Right, thickness.Bottom),
-            ThicknessSideType.Top => new Thickness(thickness.Left, 0, thickness.Right, thickness.Bottom),
-            ThicknessSideType.Right => new Thickness(thickness.Left, thickness.Top, 0, thickness.Bottom),
-            ThicknessSideType.Bottom => new Thickness(thickness.Left, thickness.Top, thickness.Right, 0),
+            LeftTopRightBottomType.Left => new Thickness(0, thickness.Top, thickness.Right, thickness.Bottom),
+            LeftTopRightBottomType.Top => new Thickness(thickness.Left, 0, thickness.Right, thickness.Bottom),
+            LeftTopRightBottomType.Right => new Thickness(thickness.Left, thickness.Top, 0, thickness.Bottom),
+            LeftTopRightBottomType.Bottom => new Thickness(thickness.Left, thickness.Top, thickness.Right, 0),
             _ => thickness,
         };
     }
