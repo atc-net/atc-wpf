f69e0bfa834a57505e366f0c88be84ccb9f60e48
davidkallesen <david@lmdk.dk>
2026-03-29 18:27:42 +0200

    feat(components): add LogCategoryTypeToResourceImageValueConverter

--- src/Atc.Wpf.Components/Monitoring/ApplicationEventEntry.cs
+++ src/Atc.Wpf.Components/Monitoring/ApplicationEventEntry.cs
@@ -1,10 +1,8 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global
-#pragma warning disable S1075
 namespace Atc.Wpf.Components.Monitoring;
 
 public class ApplicationEventEntry
 {
-    private static readonly ConcurrentDictionary<string, BitmapSource> ImageCache = new(StringComparer.Ordinal);
     private readonly LogCategoryType logCategoryType;
 
     public ApplicationEventEntry(
@@ -39,7 +37,7 @@ public class ApplicationEventEntry
         private init
         {
             logCategoryType = value;
-            LogCategoryTypeImage = GetImageByLogCategoryType(value);
+            LogCategoryTypeImage = LogCategoryTypeToResourceImageValueConverter.GetImage(value);
             LogCategoryTypeToolTip = value.GetDescription();
         }
     }
@@ -51,41 +49,4 @@ public class ApplicationEventEntry
     public string Area { get; }
 
     public string Message { get; }
-
-    private static BitmapSource GetImageByLogCategoryType(
-        LogCategoryType logCategory)
-    {
-        var imagePath = logCategory switch
-        {
-            LogCategoryType.Critical or LogCategoryType.Error
-                => "pack://application:,,,/Atc.Wpf.Components;component/Resources/Images/error.png",
-            LogCategoryType.Warning
-                => "pack://application:,,,/Atc.Wpf.Components;component/Resources/Images/warning.png",
-            LogCategoryType.Security or
-                LogCategoryType.Audit or
-                LogCategoryType.Service or
-                LogCategoryType.UI or
-                LogCategoryType.Information or
-                LogCategoryType.Debug or
-                LogCategoryType.Trace
-                => "pack://application:,,,/Atc.Wpf.Components;component/Resources/Images/information.png",
-            _ => throw new SwitchCaseDefaultException(logCategory),
-        };
-
-        return ImageCache.GetOrAdd(
-            imagePath,
-            path =>
-            {
-                var uri = new Uri(
-                    path,
-                    UriKind.Absolute);
-                var bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.UriSource = uri;
-                bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                bitmap.EndInit();
-                bitmap.Freeze();
-                return bitmap;
-            });
-    }
 }
