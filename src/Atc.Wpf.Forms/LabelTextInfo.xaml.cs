4d26e1d32c3d7011dfbee141bc170093952bface
davidkallesen <david@lmdk.dk>
2026-04-02 00:38:19 +0200

    feat(forms): add EnableCopyToClipboard property to LabelTextInfo

--- src/Atc.Wpf.Forms/LabelTextInfo.xaml.cs
+++ src/Atc.Wpf.Forms/LabelTextInfo.xaml.cs
@@ -2,6 +2,9 @@ namespace Atc.Wpf.Forms;
 
 public partial class LabelTextInfo : ILabelTextInfo
 {
+    [DependencyProperty(DefaultValue = false, PropertyChangedCallback = nameof(OnEnableCopyToClipboardChanged))]
+    private bool enableCopyToClipboard;
+
     [DependencyProperty(DefaultValue = "")]
     private string text;
 
@@ -9,4 +12,47 @@ public partial class LabelTextInfo : ILabelTextInfo
     {
         InitializeComponent();
     }
+
+    private static void OnEnableCopyToClipboardChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        var control = (LabelTextInfo)d;
+        if (control.EnableCopyToClipboard)
+        {
+            var copyMenuItem = new MenuItem
+            {
+                Header = Miscellaneous.CopyToClipboard,
+                Icon = new SvgImage
+                {
+                    Width = 16,
+                    Height = 16,
+                    Source = "/Atc.Wpf.Controls;component/Resources/Icons/clipboard.svg",
+                },
+            };
+
+            copyMenuItem.SetBinding(
+                MenuItem.IsEnabledProperty,
+                new Binding(nameof(Text))
+                {
+                    Source = control,
+                    Converter = StringNullOrEmptyToInverseBoolValueConverter.Instance,
+                });
+
+            copyMenuItem.Click += control.OnCopyToClipboardClick;
+
+            var contextMenu = new ContextMenu();
+            contextMenu.Items.Add(copyMenuItem);
+            control.ContextMenu = contextMenu;
+        }
+        else
+        {
+            control.ContextMenu = null;
+        }
+    }
+
+    private void OnCopyToClipboardClick(
+        object sender,
+        RoutedEventArgs e)
+        => System.Windows.Clipboard.SetText(Text);
 }
